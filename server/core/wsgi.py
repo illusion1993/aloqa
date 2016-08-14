40fa2dd649b7ab93659da4c6695fd38569fbe428
Vikram Singh Rathore <rathore.vikram624@gmail.com>
2016-08-14 20:44:39 +0530

    Fixed wsgi.py file

--- server/core/wsgi.py
+++ server/core/wsgi.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 """
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.settings")
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
